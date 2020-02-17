0d6eb8a3e32bee839cc5b4980353af943cda5718
JT-Stevens <Jeremy.stevnes@gmail.com>
2020-02-17 12:23:19 -0400

    overflow menu - about

--- assn2/app/src/main/java/com/nbcc/assignmnet2_richardandmortimertrivia/MainFragment.kt
+++ assn2/app/src/main/java/com/nbcc/assignmnet2_richardandmortimertrivia/MainFragment.kt
@@ -2,10 +2,8 @@ package com.nbcc.assignmnet2_richardandmortimertrivia
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentController
 import androidx.navigation.NavController
@@ -13,6 +11,7 @@ import androidx.navigation.findNavController
 import com.nbcc.assignmnet2_richardandmortimertrivia.databinding.FragmentMainBinding
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.navigation.ui.NavigationUI
 
 /**
  * A simple [Fragment] subclass.
@@ -39,7 +38,22 @@ class MainFragment : Fragment() {
             false
         )
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            navController
+        ) || super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
